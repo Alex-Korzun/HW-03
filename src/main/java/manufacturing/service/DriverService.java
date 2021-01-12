639c123284907c12c0edf10fe64ec8d852cc6bcf
Alex Korzun <koleksandr4@gmail.com>
2021-01-12 17:41:46 +0200

    added DriverService and its implementation

---
+++ src/main/java/manufacturing/service/DriverService.java
@@ -0,0 +1,17 @@
+package manufacturing.service;
+
+import manufacturing.model.Driver;
+
+import java.util.List;
+
+public interface DriverService {
+    Driver create(Driver driver);
+
+    Driver get(Long id);
+
+    List<Driver> getAll();
+
+    Driver update(Driver driver);
+
+    boolean delete(Long id);
+}
